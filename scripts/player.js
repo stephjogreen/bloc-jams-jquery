e4fed373a2274f52a2ba788983cee0050cc88a61
Steph <stephjogreen@gmail.com>
2018-08-07 11:06:06 -0500

    extra credit but doesn't work

--- scripts/player.js
+++ scripts/player.js
@@ -13,14 +13,26 @@ class Player {
   getTime() {
     return this.soundObject.getTime();
   }
-  
+
+  prettyTime(timeInSeconds) {
+  this.minutes = Math.floor(timeInSeconds / 60);
+  this.seconds = timeInSeconds % 60;
+
+  if (minutes < 10) {minutes = "0"+ minutes};
+  if (seconds < 10) {seconds = "0"+ seconds};
+
+  this.time = minutes + ":" + seconds;
+  return this.time;
+
+}
+
   playPause (song = this.currentlyPlaying) {
     if (this.currentlyPlaying !== song) {
       // Stop the currently playing sound file (even if nothing is playing)
       this.soundObject.stop();
       // Clear classes on the song that's currently playing
       this.currentlyPlaying.element.removeClass('playing paused');
-      
+
       // Update our currentlyPlaying and playState properties
       this.currentlyPlaying = song;
       this.playState = 'stopped';
@@ -37,12 +49,12 @@ class Player {
       this.currentlyPlaying.element.removeClass('playing').addClass('paused');
     }
   }
-  
+
   skipTo (percent) {
     if (this.playState !== 'playing') { return }
     this.soundObject.setTime( (percent / 100) * this.soundObject.getDuration() );
   }
-  
+
   setVolume (percent) {
     this.volume = percent;
     this.soundObject.setVolume(percent);
@@ -50,4 +62,3 @@ class Player {
 }
 
 const player = new Player();
-
